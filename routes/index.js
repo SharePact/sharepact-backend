02aa7ebfe1fa139e13cea244db53ee15790f065f
Nwokoye Chigozie <gregoflash05@gmail.com>
2024-07-16 07:42:48 +0100

    refactored router and server

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const authRoutes = require("./auth");
+const categoryRoutes = require("./category");
+const serviceRoutes = require("./service");
+const profileRoutes = require("./profile");
+const bankDetailsRoutes = require("./bankdetails");
+const groupRoutes = require("./group");
+const chatRoutes = require("./chat");
+
+class Router {
+  constructor() {
+    this.router = express.Router();
+    this.initializeRoutes();
+  }
+
+  initializeRoutes() {
+    this.router.use("/auth", authRoutes);
+    this.router.use("/api/categories", categoryRoutes);
+    this.router.use("/api/services", serviceRoutes);
+    this.router.use("/api/profile", profileRoutes);
+    this.router.use("/api", bankDetailsRoutes);
+    this.router.use("/api/groups", groupRoutes);
+    this.router.use("/api/chat", chatRoutes);
+
+    this.router.use("/", async (req, res) => {
+      res.status(200).json({ message: "Welcome to SharePact Api" });
+    });
+  }
+
+  getRouter() {
+    return this.router;
+  }
+}
+
+module.exports = Router;
--- server.js
+++ server.js
@@ -1,43 +1,14 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/auth');
-const categoryRoutes = require('./routes/category'); 
-const serviceRoutes = require('./routes/service'); 
-const profileRoutes = require('./routes/profile');
-const bankDetailsRoutes = require('./routes/bankdetails');
-const groupRoutes = require('./routes/group');
-const chatRoutes = require('./routes/chat');
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const Server = require("./middleware/index.js");
+const Router = require("./routes/index.js");
 
-const cors = require('cors');
+require("dotenv").config();
 
-require('dotenv').config();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-const app = express();
-
-
-app.use(express.json());
-app.use(cors());
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('MongoDB connected...'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
-// Routes
-app.use('/auth', authRoutes); // Use authRoutes for '/auth' routes
-app.use('/api/categories', categoryRoutes); // Mount categoryRoutes under '/api/categories'
-app.use('/api/services', serviceRoutes); // Mount serviceRoutes under '/api/services'
-app.use('/api/profile', profileRoutes); // Mount profileRoutes under '/api/profile'
-app.use('/api', bankDetailsRoutes); // Mount bankDetailsRoutes
-app.use('/api/groups', groupRoutes);
-app.use('/api/chat', chatRoutes);
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Something went wrong!' });
-});
-
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const server = new Server(new Router());
+server.startListening();
